a7dde2f8879cc39437477bdad50a7354e991a85d
Alex Will <lizardmanwill@gmail.com>
2021-10-08 20:30:43 -0400

    Logistic Curve-fit is working

--- achievement_hunting_venv/main.py
+++ achievement_hunting_venv/main.py
@@ -1,5 +1,6 @@
 from game_library_functions import *
 from app import app
+from curve_fit import *
 
 if __name__ == "__main__":
 
@@ -9,47 +10,63 @@ if __name__ == "__main__":
     ta_weights = 1.0
     gs_weights = 1.0
     weight_limit = 0.001
-    precision = 4
+    precision = 9
 
     if gamer != "":
         game_library = get_game_list(gamer)
     else:
         game_library = get_game_list()
 
-    # ACH weighting Loop #
-    ach_weights = ach_weighting_loop(game_library, ach_weights, weight_limit, precision)
+    # # ACH weighting Loop #
+    # ach_weights = ach_weighting_loop(game_library, ach_weights, weight_limit, precision)
 
-    # TA weighting Loop #
-    ta_weights = ta_weighting_loop(game_library, ta_weights, weight_limit, precision)
+    # # TA weighting Loop #
+    # ta_weights = ta_weighting_loop(game_library, ta_weights, weight_limit, precision)
 
-    # Start GS weighting Loop #
-    gs_weights = gs_weighting_loop(game_library, gs_weights, weight_limit, precision)
+    # # Start GS weighting Loop #
+    # gs_weights = gs_weighting_loop(game_library, gs_weights, weight_limit, precision)
 
-    # Alpha Values
-    ach_alpha, ta_alpha, gs_alpha = alpha_values(
-        game_library, ach_weights, ta_weights, gs_weights
+    # # Alpha Values
+    # ach_alpha, ta_alpha, gs_alpha = alpha_values(
+    #     game_library, ach_weights, ta_weights, gs_weights
+    # )
+
+    # # Create model group
+    # model_dict = {
+    #     "ach_weights": ach_weights,
+    #     "ach_alpha": ach_alpha,
+    #     "ta_weights": ta_weights,
+    #     "ta_alpha": ta_alpha,
+    #     "gs_weights": gs_weights,
+    #     "gs_alpha": gs_alpha,
+    # }
+
+    # # Establish predicted Values
+    # total_ach_gain, total_ta_gain, total_gs_gain = predict_gains(
+    #     game_library, model_dict
+    # )
+
+    # Get XY vaules
+    xy_values = get_curve_fit_xy(game_library)
+
+    # Call Curve_fit
+    constants = logit_curve_fit(xy_values)
+    print(constants[0])
+    print(constants[1])
+    print(constants[2])
+
+    # Establish Predicted Logistic Values
+
+    total_ach_gain, total_ta_gain, total_gs_gain = predict_logistic_gains(
+        game_library, constants
     )
 
-    # Create model group
-    model_dict = {
-        "ach_weights": ach_weights,
-        "ach_alpha": ach_alpha,
-        "ta_weights": ta_weights,
-        "ta_alpha": ta_alpha,
-        "gs_weights": gs_weights,
-        "gs_alpha": gs_alpha,
-    }
+    game_library.sort(key=lambda x: x.predicted_logistic_gs_gains)
 
-    # Establish predicted Values
-    total_ach_gain, total_ta_gain, total_gs_gain = predict_gains(
-        game_library, model_dict
-    )
-
-    game_library.sort(key=lambda x: x.predicted_gs_gains)
     print(game_library)
+    # print(
+    #     f"ACH: M * {ach_weights} + {ach_alpha}, TA: M * {ta_weights} + {ta_alpha}, GS: M * {gs_weights} + {gs_alpha}"
+    # )
     print(
-        f"ACH: M * {ach_weights} + {ach_alpha}, TA: M * {ta_weights} + {ta_alpha}, GS: M * {gs_weights} + {gs_alpha}"
-    )
-    print(
-        f"Total Ach gain: {total_ach_gain}, Total TA gain: {total_ta_gain}, Total GS gain: {total_gs_gain}"
+        f"Total Ach gain: {total_ach_gain:.0f}, Total TA gain: {total_ta_gain:.0f}, Total GS gain: {total_gs_gain:.0f}, Game Count : {len(game_library)}"
     )
